ccd7da01beff5ea72ffce6acf0df49f5b3878cd2
Juan C. Yamacho H <jcyamacho@gmail.com>
2023-04-24 01:12:01 +0200

    feat: send quit signal

---
+++ README.md
@@ -0,0 +1,9 @@
+# gh-stars
+
+gh-starts is a command line tool for managing your GitHub stars
+
+## Install
+
+```sh
+gh extension install jcyamacho/gh-stars
+```
--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"context"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/spf13/cobra"
 )
@@ -16,5 +19,24 @@ var rootCmd = &cobra.Command{
 
 func Execute() error {
 	ctx := context.Background()
-	return rootCmd.ExecuteContext(ctx)
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	errCh := make(chan error, 1)
+
+	go func() {
+		errCh <- rootCmd.ExecuteContext(ctx)
+	}()
+
+	select {
+	case <-c:
+		cancel()
+	case err := <-errCh:
+		return err
+	}
+
+	return <-errCh
 }
