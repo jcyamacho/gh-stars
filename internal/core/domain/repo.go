760db9cc9fefcd31ea42ffdbda853d278013b5b9
Juan C. Yamacho H <jcyamacho@gmail.com>
2023-10-31 14:49:19 +0100

    chore: extend repo model

--- internal/core/domain/repo.go
+++ internal/core/domain/repo.go
@@ -1,10 +1,13 @@
 package domain
 
 type Repo struct {
+	ID          uint64   `json:"id"`
 	Name        string   `json:"name"`
 	FullName    string   `json:"full_name"`
 	URL         string   `json:"url"`
+	HtmlURL     string   `json:"html_url"`
 	Homepage    string   `json:"homepage"`
 	Description string   `json:"description"`
 	Topics      []string `json:"topics"`
+	Language    string   `json:"language"`
 }
